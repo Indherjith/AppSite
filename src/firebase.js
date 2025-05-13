b5474bf6600058dc3eaee511495306e5828d17ca
Indherjith <buvanesifet26@gmail.com>
2025-05-13 14:24:44 +0530

    main

--- src/firebase.js
+++ src/firebase.js
@@ -22,6 +22,5 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const auth = getAuth(app);
-const analytics = getAnalytics(app);
 
 export {app,db,auth};
